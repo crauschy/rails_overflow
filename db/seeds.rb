5c93d85a1ad4162753245e50f841c0eaf9acd5ab
Cate <email": null>
2017-10-30 15:59:12 -0500

    add seed questions and users, set up homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(username: "cate", email: "cate@example.com", pw_hash: "pw" )
+User.create(username: "dave", email: "dave@example.com", pw_hash: "pw" )
+User.create(username: "lina", email: "lina@example.com", pw_hash: "pw" )
+User.create(username: "peter", email: "peter@example.com", pw_hash: "pw" )
+User.create(username: "jan", email: "jan@example.com", pw_hash: "pw" )
+
+i = 0
+while i<5
+
+  Question.create(headline: "This is question number #{i}", body: "This is the rest of question #{i}. It has a little more detail in it.", questioner_id: i, total_views: 0)
+  i+=1
+
+end
