c5537064ec1dbc1e389f8014ecbd25068e357b82
Cate <email": null>
2017-11-01 16:22:14 -0500

    preparing to push to github

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root to: 'questions#index'
   # get  '/help',    to: 'static_pages#help'
   # get  '/about',   to: 'static_pages#about'
   # get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :questions
 
